8fac29f4541bbce7cb40d3b4abb4d207d2499b5e
Mr-Tom-01 <messengerbot66@gmail.com>
2025-04-13 23:47:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "MR_X",
   botOwner: 'Hazoorbakhsh', // Your name
-  ownerNumber: '9203707709227', //Your number
+  ownerNumber: '923707709227', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
