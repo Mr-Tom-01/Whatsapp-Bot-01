a37308d02c29c4cd67e10ac33944c0dda8d5c51a
Mr-Tom-01 <messengerbot66@gmail.com>
2025-04-13 23:02:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'hazoorbakhsh',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "MR_X",
+  botOwner: 'Hazoorbakhsh', // Your name
+  ownerNumber: '923366193645', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
